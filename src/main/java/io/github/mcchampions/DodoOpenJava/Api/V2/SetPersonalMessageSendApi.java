663a7d7e51953bf022098b99eca1686f69d1468a
kled <hold_lijun@163.com>
2023-03-14 20:59:50 +0800

    update

--- src/main/java/io/github/mcchampions/DodoOpenJava/Api/V2/SetPersonalMessageSendApi.java
+++ src/main/java/io/github/mcchampions/DodoOpenJava/Api/V2/SetPersonalMessageSendApi.java
@@ -1,6 +1,5 @@
 package io.github.mcchampions.DodoOpenJava.Api.V2;
 
-import io.github.mcchampions.DodoOpenJava.Card.Card;
 import io.github.mcchampions.DodoOpenJava.Utils.NetUtil;
 import org.json.JSONObject;
 
@@ -8,15 +7,18 @@ import java.io.IOException;
 
 public class SetPersonalMessageSendApi {
     public static String url, param;
-    public static JSONObject personalMessageSendCard(String authorization, String islandSourceId, String dodoSourceId, Integer messageType, Card card) throws IOException {
+    public static JSONObject personalMessageSendCard(String authorization, String islandSourceId, String dodoSourceId, Integer messageType, String msg) throws IOException {
+        JSONObject content = new JSONObject();
+        content.put("content",msg);
+        JSONObject paramJson = new JSONObject();
+        paramJson.put("islandSourceId",islandSourceId);
+        paramJson.put("dodoSourceId",dodoSourceId);
+        paramJson.put("messageType",messageType);
+        paramJson.put("messageBody",content);
+
         url = "https://botopen.imdodo.com/api/v2/personal/message/send";
-        param = "{" +
-                "    \"islandSourceId\": \"" + islandSourceId + "\"," +
-                "    \"dodoSourceId\": \"" + dodoSourceId + "\"," +
-                "    \"messageType\": \"" + messageType + "\"," +
-                "    \"messageBody\": \"" + card.toJSONObject().toString() + "\"" +
-                "}";
-        return new JSONObject(NetUtil.sendRequest(param, url, authorization));
+
+        return new JSONObject(NetUtil.sendRequest(paramJson.toString(), url, authorization));
     }
 
     public static JSONObject personalMessageSendMessage(String authorization, String islandSourceId, String dodoSourceId, Integer messageType, String message) throws IOException {
